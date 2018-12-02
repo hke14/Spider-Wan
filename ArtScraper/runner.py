f7244e0ec3926ebb3a441428c8759fab278e617b
hke14@mail.aub.edu <hke14@mail.aub.edu>
2018-12-02 23:15:15 +0200

    first commit

--- ArtScraper/runner.py
+++ ArtScraper/runner.py
@@ -6,19 +6,19 @@ while True:
 
 
 
-    os.system("scrapy crawl craww")
-    os.system("scrapy crawl crawly")
+    # os.system("scrapy crawl craww")
+    # os.system("scrapy crawl crawly")
     os.system("scrapy crawl craw")
-    os.system("scrapy crawl c")
-    os.system("scrapy crawl y")
-    os.system("scrapy crawl george")
-    os.system("scrapy crawl sport")
-    os.system("scrapy crawl middle")
-    os.system("scrapy crawl business")
-    os.system("scrapy crawl tech")
-    os.system("scrapy crawl bayans")
-    os.system("scrapy crawl bayanw")
-    os.system("scrapy crawl bayanb")
+    # os.system("scrapy crawl c")
+    # os.system("scrapy crawl y")
+    # os.system("scrapy crawl george")
+    # os.system("scrapy crawl sport")
+    # os.system("scrapy crawl middle")
+    # os.system("scrapy crawl business")
+    # os.system("scrapy crawl tech")
+    # os.system("scrapy crawl bayans")
+    # os.system("scrapy crawl bayanw")
+    # os.system("scrapy crawl bayanb")
     time.sleep(5)
 
 
